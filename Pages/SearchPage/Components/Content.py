2eec21ea89e1d4a75ae544671cee49a8800b0f14
KhadapkarS20 <shreyaskhadapkar@gmail.com>
2020-04-27 19:49:44 +0530

    Browse Thoda

--- Pages/SearchPage/Components/Content.py
+++ Pages/SearchPage/Components/Content.py
@@ -7,17 +7,34 @@ class Content(tk.Frame):
         tk.Frame.__init__(self,master,*args,**kwargs)
 
         self.scrollableFrame = ScrollableFrame(self)
-
-        for j,i in enumerate(data):
-            self.music = MusicFrame(self.scrollableFrame.scrollable_frame,
-                                        title=i['title'],
-                                        album=i['genre'],
-                                        artist=i['artist'],
-                                        url=i['location'])
-            self.line = tk.Frame(self.scrollableFrame.scrollable_frame,
-                                    bg='#333333')
-            self.music.grid(row=j*2, column=0, sticky='nsew', padx=(30, 15))
-            self.line.grid(row=j*2 + 1 ,column =0 ,sticky='nsew')
+        print(data)
+        if len(data) == 1 and data[0] == 'not found':
+            self.label = tk.Label(self,
+                                  text='Song not in the list',
+                                  background='#181818',
+                                  foreground='#999999',
+                                  font=self.support
+                                  )
+            self.label.grid(row=0, column=0)
+        elif len(data) == 0:
+            self.label = tk.Label(self,
+                                  text='Search Results Will appear here',
+                                  background='#181818',
+                                  foreground='#999999',
+                                  font=self.support
+                                  )
+            self.label.grid(row=0, column=0)
+        else:
+            for j,i in enumerate(data):
+                self.music = MusicFrame(self.scrollableFrame.scrollable_frame,
+                                            title=i['title'],
+                                            album=i['genre'],
+                                            artist=i['artist'],
+                                            url=i['location'])
+                self.line = tk.Frame(self.scrollableFrame.scrollable_frame,
+                                        bg='#333333')
+                self.music.grid(row=j*2, column=0, sticky='nsew', padx=(30, 15))
+                self.line.grid(row=j*2 + 1 ,column =0 ,sticky='nsew')
         
         self.scrollableFrame.grid(row=0,column=0,sticky='nsew')
         self.grid_rowconfigure(0,weight=1)
--- Pages/SearchPage/SearchPage.py
+++ Pages/SearchPage/SearchPage.py
@@ -1,36 +1,43 @@
 import tkinter as tk
-from .Components.FilterFrame import FilterFrame
+# from .Components.FilterFrame import FilterFrame
 from Pages.MusicPage.Components.TitleFrame import TitleFrame
 from .Components.MusicFrame import MusicFrame
 from .Components.Content import Content
+import pyglet
+import tkinter.font as tkfont
 
 class SearchPage(tk.Frame):
     def __init__(self, master, *args, **kwargs):
         tk.Frame.__init__(self, master, *args, **kwargs)
-        self['background'] = "yellow"
+        self['background'] = "#000000"
 
-        self.titleFrame = tk.Frame(self,background="blue")
-        self.contentFrame = tk.Frame(self,background="red")
+        self.titleFrame = tk.Frame(self,background="#999999")
+        self.contentFrame = tk.Frame(self,background="#191919")
+
+        pyglet.font.add_file('fonts/Play/Play-Bold.ttf')
+        play = tkfont.Font(family="Play", size=35, weight="bold")
         
-        self.heading = tk.Label(self.titleFrame,text="Search",bg="#000000",foreground='white')
+        self.heading = tk.Label(self.titleFrame,text="Search",bg="#000000",foreground='white',font=play,padx=20)
 
-        self.filterFrame = FilterFrame(self.contentFrame,data=self.data())
+        # self.filterFrame = FilterFrame(self.contentFrame,data=self.data())
         self.labelTitleFrame = TitleFrame(self.contentFrame)
         self.content = Content(self.contentFrame,data=self.data())
 
 
-        self.titleFrame.grid(row=0, column=0 , sticky='nsew')
+        self.titleFrame.grid(row=0, column=0 , sticky='nsw')
         self.heading.grid(row=0, column=0 , sticky='nsew')
         self.contentFrame.grid(row=1,column=0,sticky='nsew')
-        self.filterFrame.grid(row=0,column=0,sticky='nsew')
-        self.labelTitleFrame.grid(row=1,column=0,sticky='nsew')
+        # self.filterFrame.grid(row=0,column=0,sticky='nsew')
+        self.labelTitleFrame.grid(row=0,column=0,sticky='nsew')
 
-        self.grid_rowconfigure(0,weight=3)
+        self.titleFrame.grid_rowconfigure(0,weight=1)
+        self.titleFrame.grid_columnconfigure(0,weight=1)
+        self.grid_rowconfigure(0,weight=1)
         self.grid_rowconfigure(1,weight=10)
         self.grid_columnconfigure(0,weight=1)
-        self.contentFrame.grid_rowconfigure(0,weight=2)
-        self.contentFrame.grid_rowconfigure(1,weight=1)
-        self.contentFrame.grid_rowconfigure(2,weight=100)
+        # self.contentFrame.grid_rowconfigure(0,weight=2)
+        self.contentFrame.grid_rowconfigure(0,weight=1)
+        self.contentFrame.grid_rowconfigure(1,weight=55)
         self.contentFrame.grid_columnconfigure(0,weight=1)
         # self.contentFrame.grid_columnconfigure(0,weight=1)
 
